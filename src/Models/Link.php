640fb339355aa294c0e565bca47189ba643df45f
Toby Twigger <tobytwigger1@gmail.com>
2020-05-02 10:15:03 +0100

    Fix date serialisation

--- src/Models/Link.php
+++ src/Models/Link.php
@@ -39,6 +39,17 @@ class Link extends Model implements LinkModelContract
      */
     protected $normaliser;
 
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
